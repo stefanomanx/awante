e769f657a15aec37cd6ef0caa5bb07b8dc4ce10e
Stefano Mandolesi <stefano232@gmail.com>
2022-03-09 16:21:54 -0300

    favorite button heart

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,5 +6,18 @@ class FavoritesController < ApplicationController
     @favorite.artist = @artist
     @favorite.user = current_user
     @favorite.save
+    respond_to do |format|
+      format.js {render inline: "location.reload();" }
+    end
+  end
+
+  def destroy
+    @artist = Artist.find(params[:artist_id])
+    @favorite.artist = @artist
+    @favorite.user = current_user
+    @favorite.destroy
+    respond_to do |format|
+      format.js {render inline: "location.reload();" }
+    end
   end
 end
